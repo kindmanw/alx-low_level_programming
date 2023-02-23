80317310f0db6d86c4ed59310f38d43a2f274cf6
William Kindness <wkindman4@gmail.com>
2023-02-23 11:32:51 -0800

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,13 +5,12 @@
  *
  * Return: void
  */
+
 void print_numbers(void)
 {
-	char c;
+	int a;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-		_putchar(c);
-	{
+	for (a = 48; a < 58; a++)
+		_putchar(a);
 	_putchar('\n');
 }
